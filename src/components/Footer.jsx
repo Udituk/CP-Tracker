6670b0778d5d03fda27747261d762805bd8b9127
Udituk <udit.kindore@gmail.com>
2021-07-01 14:09:19 +0530

    Header, Footer components
    Header, Footer components added.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,20 @@ import React from "react";
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer>
-      <p>Copyright ⓒ {year}</p>
+    <footer class="page-footer font-small blue">
+      <div class="footer-copyright text-center py-3">
+        <p>
+          Copyright ⓒ {year}{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Udituk/CP-Tracker"
+            style={{color:"black",textDecoration:"none"}}
+          >
+            CP-Tracker
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,35 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>
-
-      </h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          CP-Tracker
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" rel="noreferrer" href="#">
+              Profile <span class="sr-only">(current)</span>
+            </a>
+            <a class="nav-item nav-link" href="#">
+              Contests
+            </a>
+            <a class="nav-item nav-link" href="#">
+              About Us
+            </a>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
